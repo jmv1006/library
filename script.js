4a8173b13f4b65514466925329e679f62a725bcf
Jonathan Andrade Valenzuela <jmv1006@gmail.com>
2021-11-01 09:41:21 -0700

    index.html

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let title;
 let author;
+let titleInput;
+let authorInput;
+let newBook
 
-function storeValues() {
-    let titleInput = document.getElementById('titleInput').value;
-    let authorInput = document.getElementById('authorInput').value;
-    title = titleInput;
-    author = authorInput;
-}
-
-
+function saveInputs() {
+    titleInput = document.getElementById('titleInput').value;
+    authorInput = document.getElementById('authorInput').value;
+    assignInputsToBook();
+};
 
 let myLibrary = [];
 
@@ -23,8 +23,15 @@ function Book(title, author, pages) {
 
 //link title, author, and page number to user input. Ex,
 // newBook = new Book('userTitleInput', 'userAuthorInput', 'userPageNumInput');
+/*
 let newBook = new Book('The Great Gatsby', 'J Fitzgerald', 532);
 newBook.info();
+*/
+
+function assignInputsToBook() {
+    newBook = new Book(titleInput, authorInput, 532);
+    addBookToLibrary();
+}
 
 
 //this sends the new book to the myLibrary array
@@ -32,5 +39,4 @@ function addBookToLibrary() {
     myLibrary.push(newBook);
 }
 
-addBookToLibrary();
 
